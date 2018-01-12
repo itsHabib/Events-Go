6eb3baf837dd8eaf60799eba0f41e63afffa3eb9
itsHabib <michaelhabib1868@gmail.com>
2018-01-11 17:24:25 -0800

    create models and interface for persistence layer

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+// TODO separate handlers  into its own packages
+
+type DatabaseHandler interface {
+	AddEvent(Event) ([]byte, error)
+	FindEvent([]byte) (Event, error)
+	FindEventByName(string) (Event, error)
+	FindAllAvailableEvents() ([]Event, error)
+}
 
 type eventServiceHandler struct{}
 
@@ -23,9 +33,13 @@ func (eh *eventServiceHandler) newEventHandler(w http.ResponseWriter,
 }
 
 func main() {
+	log.Fatal(ServeAPI(":8081"))
+}
+
+func ServeAPI(endpoint string) error {
+	handler := &eventServiceHandler{}
 	r := mux.NewRouter()
 	eventsRouter := r.PathPrefix("/events").Subrouter()
-	handler := eventServiceHandler{}
 
 	// Searching for events via id or name
 	// events/id/123 or /events/name/rock
@@ -40,6 +54,6 @@ func main() {
 	// /events
 	eventsRouter.Methods("POST").Path("").HandlerFunc(handler.newEventHandler)
 
-	http.ListenAndServe(":8181", r)
+	return http.ListenAndServe(endpoint, r)
 
 }
