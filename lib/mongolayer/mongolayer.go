bad8e41c9cd6dc4324b33e4854f3928d99a2aa33
itsHabib <michaelhabib1868@gmail.com>
2018-01-11 19:44:47 -0800

    create persistence layer, add mongodblayer

---
+++ lib/mongolayer/mongolayer.go
@@ -0,0 +1,70 @@
+package mongolayer
+
+import (
+	"github.com/itsHabib/cloud-native-go/lib/persistence"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+const (
+	DB     = "events-go"
+	USERS  = "users"
+	EVENTS = "events"
+)
+
+type MongoDBLayer struct {
+	session *mgo.Session
+}
+
+// NewMongoDBLayer Creates a connection between the mongodb and returns the
+// session tied to MongoDBLayer
+func NewMongoDBLayer(connection string) (*MongoDBLayer, error) {
+	s, err := mgo.Dial(connection)
+	if err != nil {
+		return nil, err
+	}
+	return &MongoDBLayer{
+		session: s,
+	}, err
+}
+
+func (mgoLayer *MongoDBLayer) AddEvent(e persistence.Event) ([]byte, error) {
+	s := mgoLayer.getFreshSession()
+	defer s.Close()
+
+	if !e.ID.Valid() {
+		e.ID = bson.NewObjectId()
+	}
+	if !e.Location.ID.Valid() {
+		e.Location.ID = bson.NewObjectId()
+	}
+	return []byte(e.ID), s.DB(DB).C(EVENTS).Insert(e)
+}
+
+func (mgoLayer *MongoDBLayer) getFreshSession() *mgo.Session {
+	return mgoLayer.session.Copy()
+}
+
+func (mgoLayer *MongoDBLayer) FindEvent(id []byte) (persistence.Event, error) {
+	s := mgoLayer.getFreshSession()
+	defer s.Close()
+	e := persistence.Event{}
+	err := s.DB(DB).C(EVENTS).FindId(bson.ObjectId(id)).One(&e)
+	return e, err
+}
+
+func (mgoLayer *MongoDBLayer) FindEventByName(name string) (persistence.Event, error) {
+	s := mgoLayer.getFreshSession()
+	defer s.Close()
+	e := persistence.Event{}
+	err := s.DB(DB).C(EVENTS).Find(bson.M{"name": name}).One(&e)
+	return e, err
+}
+
+func (mgoLayer *MongoDBLayer) FindAllAvailableEvents() ([]persistence.Event, error) {
+	s := mgoLayer.getFreshSession()
+	defer s.Close()
+	events := []persistence.Event{}
+	err := s.DB(DB).C(EVENTS).Find(nil).All(&events)
+	return events, err
+}
