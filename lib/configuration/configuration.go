8be4d67658062c94ad48cfc3b1e859c360c141fc
itsHabib <michaelhabib1868@gmail.com>
2018-01-12 23:22:42 -0800

    create tls endpoint default in configuration layer, call serveAPI with tlsendpoint

--- lib/configuration/configuration.go
+++ lib/configuration/configuration.go
@@ -12,12 +12,14 @@ var (
 	DBTypeDefault       = dblayer.DBTYPE("mongodb")
 	DBConnectionDefault = "mongodb://127.0.0.1"
 	RestfulEPDefault    = "localhost:8081"
+	RestfulTLSEPDefault = "localhost:9091"
 )
 
 type ServiceConfig struct {
-	Databasetype    dblayer.DBTYPE `json:"databasetype"`
-	DBConnection    string         `json:"dbconnection"`
-	RestfulEndpoint string         `json:"restfulapi_endpoint"`
+	Databasetype       dblayer.DBTYPE `json:"databasetype"`
+	DBConnection       string         `json:"dbconnection"`
+	RestfulEndpoint    string         `json:"restfulapi_endpoint"`
+	RestfulTLSEndpoint string         `json:"restful_tlsendpoint`
 }
 
 func ExtractConfiguration(filename string) (ServiceConfig, error) {
@@ -25,6 +27,7 @@ func ExtractConfiguration(filename string) (ServiceConfig, error) {
 		DBTypeDefault,
 		DBConnectionDefault,
 		RestfulEPDefault,
+		RestfulTLSEPDefault,
 	}
 	file, err := os.Open(filename)
 	if err != nil {
